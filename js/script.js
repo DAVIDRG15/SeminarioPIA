b53c69ef966ed498c6baffde1170f3099ff588cb
David Resendiz <132948326+DAVIDRG15@users.noreply.github.com>
2024-04-26 19:12:02 -0600

    add functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-console.log('Script funcionando');
+const conversationDiv = document.getElementById('conversation');
+const userInput = document.getElementById('userInput');
+const btnSend = document.getElementById('btnSend');
+
+btnSend.addEventListener('click', () => {
+    const message = userInput.value.trim();
+
+    if (message !== '') {
+        userInput.value = '';
+        appendMessage('user', message);
+        respondToUser(message);
+    } else {
+        console.log('Por favor, escribe un mensaje válido.');
+    }
+});
+
+
+function appendMessage(sender, message) {
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('row');
+    const colClass = sender === 'user' ? 'justify-content-end' : 'justify-content-start';
+    messageDiv.innerHTML = `
+        <div class="col-12 col-md-6 mb-4">
+            <div class="bg-${sender === 'user' ? 'primary text-white rounded-start' : 'secondary text-white rounded-end'} p-3">
+                ${message}
+            </div>
+        </div>
+    `;
+    messageDiv.classList.add(colClass); // Agregar la clase de alineación a messageDiv
+    conversationDiv.appendChild(messageDiv);
+}
+
+
+
+function respondToUser(message) {
+    let response = '';
+
+    switch (message.toLowerCase()) {
+        case 'hola':
+            response = '¡Hola! ¿En qué puedo ayudarte?';
+            break;
+        case 'adios':
+            response = '¡Hasta luego!';
+            break;
+        default:
+            response = 'Lo siento, no entiendo. ¿Puedes ser más específico?';
+            break;
+    }
+
+    appendMessage('assistant', response);
+}
